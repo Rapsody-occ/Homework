bc0302ad8f0fbc9aee6553b92e3122d8342f624b
USER <victorunifun@gmail.com>
2022-04-12 23:04:24 +0300

    try to commit

--- lessonOne/main.swift
+++ lessonOne/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
+//
+//var name = "Vasilii"
+//name = "Petr"
+//print(name)
+//let birthDate = "01.01.1980"
+//
+//
+//let b = Float.pi + 93484245
+//print(b)
 
+var b = ["123" : 123, "321" : 321]
+b["123"] = nil
+print(b)
